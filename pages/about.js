73381b8828555b554e1d3c73f7bfd4cbca30926c
Alexandre Bédard <alexandre.bedard123@gmail.com>
2022-06-22 20:21:23 -0400

    First commit

---
+++ pages/about.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+const about = () => {
+    return (
+        <div>
+            <Head>
+                <title>About</title>
+            </Head>
+            <h1>About</h1>
+            <a href={'/'}>Back</a>
+        </div>
+    )
+}
+
+export default about
